c1c137ba4441709d6a3333c69180bfb80f048b34
Aliyv019 <tahiraliyev2006@gmail.com>
2024-08-26 20:28:43 +0400

    added number functionality with add and subtract buttons

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './card.css'
 
 export default function Card({name,age,profession}) {
     const testnumber=0
+    const [num, setNum] = useState(0)
 
   return (
     <>
         <h2>{name}</h2>
-        <p>Bu adamin {age} yasi var. Isi gucu {profession}dir. {testnumber}</p>
+        <p>Bu adamin {age} yasi var. Isi gucu {profession}dir. {num}</p>
         <div className='card_btns'>
-            <button>Add</button>
-            <button>Subtract</button>
+            <button onClick={()=>setNum(num+1)}>Add</button>
+            <button onClick={()=>setNum(num-1)}>Subtract</button>
         </div>
     </>
   )
